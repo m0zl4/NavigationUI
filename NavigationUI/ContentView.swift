99c21d0a92e5a26e824a9b29a5aaee5ea0e62e2d
m0zl4 <zayuka040104@gmail.com>
2021-01-29 10:26:08 +0700

    Initial Commit

---
+++ NavigationUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationUI
+//
+//  Created by muhammad abdul latief on 29/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
