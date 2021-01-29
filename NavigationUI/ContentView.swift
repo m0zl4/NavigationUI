006fd77e58e850fd26ab74cf8150358ffa79ef0b
m0zl4 <68722896+m0zl4@users.noreply.github.com>
2021-01-29 11:10:18 +0700

    Update ContentView.swift

--- NavigationUI/ContentView.swift
+++ NavigationUI/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
 }
 
@@ -19,3 +18,57 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Home : View {
+    var body: some View{
+        NavigationView{
+            Text("Hello saya Latiev")
+            
+            .navigationBarItems(
+                // logo_yt
+                leading:
+                HStack{
+                    Button(action : {print("Hello World")}){
+                        Image("youtube_logo")
+                        .renderingMode(.original)
+                        .resizable()
+                        .frame(width : 90, height: 20)
+                    }
+                    
+                },
+                
+                // Profile dll
+                trailing:
+                HStack(spacing : 20){
+                    Button(action : {print("Hello World")}){
+                        Image(systemName: "tray")
+                            .foregroundColor(Color.black)
+                        
+                    }
+                    
+                    Button(action : {print("Hello World")}){
+                        Image(systemName: "video.fill")
+                            .foregroundColor(Color.black)
+                        
+                    }
+                    
+                    Button(action : {print("Hello World")}){
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(Color.black)
+                        
+                    }
+                    
+                    Button(action : {print("Hello World")}){
+                        Image("profile")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width : 20, height: 20)
+                            .clipShape(Circle())
+                        
+                    }
+                }
+            )
+        }
+    }
+}
+
